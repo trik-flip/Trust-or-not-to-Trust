124b6aec4e140e8d1b9129317e5254f8e8bdef22
Philip Bollen <flip.philip@hotmail.com>
2022-11-22 17:17:58 +0100

    Remove NTCM class

--- to_trust/methods/Example/example_ntcm.py
+++
@@ -1,6 +0,0 @@
-from to_trust.testbed.agents import NovelTrustComputingMethod, Witness
-
-
-class ExampleNtcm(NovelTrustComputingMethod):
-    def calc(self, score: float, testimonies: dict[Witness, float]) -> float:
-        return score + sum(testimonies.values()) / len(testimonies)
