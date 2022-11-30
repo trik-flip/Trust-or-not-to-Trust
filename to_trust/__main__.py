33f8463706f70f62dc1e79cd25fb6f4ca9728054
Philip Bollen <flip.philip@hotmail.com>
2022-11-30 08:54:53 +0100

    Refactor

--- to_trust/__main__.py
+++ to_trust/__main__.py
@@ -1,54 +1,82 @@
-from .testbed import Simulation
-from .methods import Act
-from .scenarios import HostileEnvironment, StartLying
 import matplotlib.pyplot as plt
 
+from .methods import Act
+from .scenarios import HostileEnvironment, StartLying
+from .testbed import Simulation
+from .util import profiler
 scenario = StartLying(
     witness_amount=5,
     consumer_amount=5,
     provider_amount=20,
-    provider_options={"chance": 0.7, "l_quality": 0.8, "l_cost": 0.3, "u_cost": 0.6},
+    provider_options={"chance": 0.7, "l_quality": 0.8,
+                      "l_cost": 0.2, "u_cost": 0.5},
 )
 
+epochs = 5
 ntcm = Act
-sim = Simulation(scenario, ntcm, 1000)
 
+simulation = Simulation(scenario, ntcm, 250)
 
-for c, p in sim.runs(4):
+profiler.start()
+for consumers, providers in simulation.runs(epochs):
+    profiler.start("single run")
     print()
-    print(f"Consumer Average: {sum(sum(c[_c]) for _c in c )/len(c):.2f}")
-    print(f"Provider Average: {sum(sum(p[_p]) for _p in p)/len(p):.2f}")
-    c_list = {_c: [sum(_v[:v]) for v in range(len(_v))] for _c, _v in c.items()}
-    best_c = max(c_list.keys(), key=lambda c: c_list[c][-1])
-    p_list = {_p: [sum(_v[:v]) for v in range(len(_v))] for _p, _v in p.items()}
-    best_p = max(p_list.keys(), key=lambda p: p_list[p][-1])
-    c_label_set = False
-    p_label_set = False
-    average_p = [sum(x) / len(p_list.values()) for x in zip(*p_list.values())]
-    average_c = [sum(x) / len(c_list.values()) for x in zip(*c_list.values())]
-    plt.plot(average_c, "-", linewidth=4, label="Average Consumer")
-    plt.plot(average_p, ":", linewidth=4, label="Average Provider")
+    print(
+        f"Consumer Average: {sum(sum(consumers[_c]) for _c in consumers )/len(consumers):.2f}")
+    print(
+        f"Provider Average: {sum(sum(providers[_p]) for _p in providers)/len(providers):.2f}")
 
-    for _c in c:
-        if best_c == _c:
-            plt.plot(c_list[_c], "-b", label="Best Consumer")
+    profiler.start("main-1")
+    consumer_list = {_c: [sum(_v[:v]) for v in range(len(_v))]
+                     for _c, _v in consumers.items()}
+    best_consumer = max(consumer_list.keys(),
+                        key=lambda c: consumer_list[c][-1])
+    profiler.switch("main-1", "main-2")
+
+    producer_list = {_p: [sum(_v[:v]) for v in range(len(_v))]
+                     for _p, _v in providers.items()}
+    best_provider = max(producer_list.keys(),
+                        key=lambda p: producer_list[p][-1])
+    profiler.switch("main-2", "main-3")
+
+    consumer_label_set = False
+    provider_label_set = False
+
+    average_producer = [sum(x) / len(producer_list.values())
+                        for x in zip(*producer_list.values())]
+    average_consumer = [sum(x) / len(consumer_list.values())
+                        for x in zip(*consumer_list.values())]
+
+    profiler.stop("main-3")
+
+    plt.plot(average_consumer, "-", linewidth=4, label="Average Consumer")
+    plt.plot(average_producer, ":", linewidth=4, label="Average Provider")
+
+    for consumer in consumers:
+        if best_consumer == consumer:
+            plt.plot(consumer_list[consumer], "-b", label="Best Consumer")
         else:
-            if not c_label_set:
-                plt.plot(c_list[_c], "-g", label="Consumer")
-                c_label_set = True
+            if not consumer_label_set:
+                plt.plot(consumer_list[consumer], "-g", label="Consumer")
+                consumer_label_set = True
             else:
-                plt.plot(c_list[_c], "-g")
-    for _p in p:
-        if best_p == _p:
-            plt.plot(p_list[_p], ":c", label="Best Provider")
+                plt.plot(consumer_list[consumer], "-g")
+    for provider in providers:
+        if best_provider == provider:
+            plt.plot(producer_list[provider], ":c", label="Best Provider")
         else:
-            if not p_label_set:
-                plt.plot(p_list[_p], ":r", label="Provider")
-                p_label_set = True
+            if not provider_label_set:
+                plt.plot(producer_list[provider], ":r", label="Provider")
+                provider_label_set = True
             else:
-                plt.plot(p_list[_p], ":r")
+                plt.plot(producer_list[provider], ":r")
     plt.title("Trust Simulation")
     plt.xlabel("Time step")
     plt.ylabel("Utility")
     plt.legend()
     plt.show()
+    profiler.stop("single run")
+
+profiler.stop()
+
+profiler.show(False, profiler.ms)
