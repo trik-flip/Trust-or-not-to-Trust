9607d95938a8dfe81a8e1cef52450f57bf9182fb
Philip Bollen <flip.philip@hotmail.com>
2023-01-25 12:18:07 +0100

    Add new running settings + running all scenarios

--- to_trust/__main__.py
+++ to_trust/__main__.py
@@ -1,125 +1,211 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from to_trust.util import profiler
 
-from metrics import MetricSystem
-from settings import epochs, line_alpha, plot_average, runs, simulation, plot_run
-from util import profiler
+from to_trust.methods import ITEA, MET, Act, Travos
+from to_trust.metrics import MetricSystem
+from to_trust.settings import (
+    epochs,
+    line_alpha,
+    ntcm_type,
+    plot_average,
+    plot_run,
+    runs,
+    scenarios,
+    simulation,
+)
+from to_trust.testbed.simulation import Simulation
 
-# TODO (Philip): run + rename for every scenario in settings
-if __name__ == '__main__':
+
+def name_of(ntcm):
+    if ntcm is Travos:
+        return "Travos"
+    elif ntcm is Act:
+        return "ACT-RL"
+    elif ntcm is ITEA:
+        return "ITEA"
+    elif ntcm is MET:
+        return "MET"
+    else:
+        raise Exception("Method doesn't exist")
+
+
+if __name__ == "__main__":
     sensor = MetricSystem()
     profiler.start()
+    print(f"Running {name_of(ntcm_type)}")
 
-    experiment_name = "TRAVOS_HOSTILE"
+    consumer_utility_data = pd.DataFrame(
+        [],
+        columns=[
+            "overall_run_index",
+            "simulation_run_index",
+            "consumer_index",
+            "utility",
+        ],
+    )
+    provider_utility_data = pd.DataFrame(
+        [],
+        columns=[
+            "overall_run_index",
+            "simulation_run_index",
+            "provider_index",
+            "utility",
+        ],
+    )
 
-    consumer_utility_data = pd.DataFrame([], columns=["overall_run_index",
-                                                      "simulation_run_index",
-                                                      "consumer_index",
-                                                      "utility"])
-    provider_utility_data = pd.DataFrame([], columns=["overall_run_index",
-                                                      "simulation_run_index",
-                                                      "provider_index",
-                                                      "utility"])
+    consumer_mae_data = pd.DataFrame(
+        [],
+        columns=["overall_run_index", "simulation_run_index", "consumer_index", "mae"],
+    )
+    for s in scenarios:
+        simulation = Simulation(scenarios[s], ntcm_type, runs)
 
-    consumer_mae_data = pd.DataFrame([], columns=["overall_run_index",
-                                                  "simulation_run_index",
-                                                  "consumer_index",
-                                                  "mae"])
+        for overall_run_index, (consumers, providers) in enumerate(
+            simulation.runs(epochs, printing=True)
+        ):
+            sensor.measure(consumers, providers, runs)
+            profiler.start("single run")
 
-    for overall_run_index, (consumers, providers) in enumerate(simulation.runs(epochs, printing=True)):
-        sensor.measure(consumers, providers, runs)
-        profiler.start("single run")
+            # collect data for export
+            consumer_utility_data_one_run = (
+                pd.DataFrame(consumers.values())
+                .stack()
+                .reset_index()
+                .rename(
+                    columns={
+                        "level_0": "consumer_index",
+                        "level_1": "simulation_run_index",
+                        0: "utility",
+                    }
+                )
+            )
+            consumer_utility_data_one_run["overall_run_index"] = overall_run_index
+            consumer_utility_data = pd.concat(
+                [consumer_utility_data, consumer_utility_data_one_run],
+                ignore_index=True,
+            )
 
-        # collect data for export
-        consumer_utility_data_one_run = pd.DataFrame(consumers.values()).stack().reset_index().rename(
-            columns={"level_0": "consumer_index", "level_1": "simulation_run_index", 0: "utility"}
-        )
-        consumer_utility_data_one_run['overall_run_index'] = overall_run_index
-        consumer_utility_data = pd.concat([consumer_utility_data, consumer_utility_data_one_run], ignore_index=True)
+            provider_utility_data_one_run = (
+                pd.DataFrame(providers.values())
+                .stack()
+                .reset_index()
+                .rename(
+                    columns={
+                        "level_0": "provider_index",
+                        "level_1": "simulation_run_index",
+                        0: "utility",
+                    }
+                )
+            )
+            provider_utility_data_one_run["overall_run_index"] = overall_run_index
+            provider_utility_data = pd.concat(
+                [provider_utility_data, provider_utility_data_one_run],
+                ignore_index=True,
+            )
 
-        provider_utility_data_one_run = pd.DataFrame(providers.values()).stack().reset_index().rename(
-            columns={"level_0": "provider_index", "level_1": "simulation_run_index", 0: "utility"}
-        )
-        provider_utility_data_one_run['overall_run_index'] = overall_run_index
-        provider_utility_data = pd.concat([provider_utility_data, provider_utility_data_one_run], ignore_index=True)
+            print()
+            print(f"Consumer Average: {sensor.average(consumers):.2f}")
+            print(f"Provider Average: {sensor.average(providers):.2f}")
 
-        print()
-        print(f"Consumer Average: {sensor.average(consumers):.2f}")
-        print(f"Provider Average: {sensor.average(providers):.2f}")
+            consumer_list = {
+                _c: [sum(_v[:v]) for v in range(len(_v))]
+                for _c, _v in consumers.items()
+            }  # get the compounded list per consumer which contains the values
+            best_consumer = max(
+                consumer_list.keys(), key=lambda c: consumer_list[c][-1]
+            )
 
-        consumer_list = {
-            _c: [sum(_v[:v]) for v in range(len(_v))] for _c, _v in consumers.items()
-        }  # get the compounded list per consumer which contains the values
-        best_consumer = max(consumer_list.keys(), key=lambda c: consumer_list[c][-1])
+            provider_list = {
+                _p: [sum(_v[:v]) for v in range(len(_v))]
+                for _p, _v in providers.items()
+            }  # Same but then for the providers
+            best_provider = max(
+                provider_list.keys(), key=lambda p: provider_list[p][-1]
+            )
 
-        provider_list = {
-            _p: [sum(_v[:v]) for v in range(len(_v))] for _p, _v in providers.items()
-        }  # Same but then for the providers
-        best_provider = max(provider_list.keys(), key=lambda p: provider_list[p][-1])
+            consumer_label_set = False
+            provider_label_set = False
 
-        consumer_label_set = False
-        provider_label_set = False
+            average_provider = [
+                sum(x) / len(provider_list.values())
+                for x in zip(*provider_list.values())
+            ]
+            average_consumer = [
+                sum(x) / len(consumer_list.values())
+                for x in zip(*consumer_list.values())
+            ]
 
-        average_provider = [
-            sum(x) / len(provider_list.values()) for x in zip(*provider_list.values())
-        ]
-        average_consumer = [
-            sum(x) / len(consumer_list.values()) for x in zip(*consumer_list.values())
-        ]
+            if plot_average:
+                plt.plot(average_consumer, "-", lw=4, label="Average Consumer")
+                plt.plot(average_provider, ":", lw=4, label="Average Provider")
 
-        if plot_average:
-            plt.plot(average_consumer, "-", lw=4, label="Average Consumer")
-            plt.plot(average_provider, ":", lw=4, label="Average Provider")
-
-        for consumer in consumers:
-            if best_consumer == consumer:
-                plt.plot(consumer_list[consumer], "-b", label="Best Consumer")
-            else:
-                if not consumer_label_set:
-                    plt.plot(
-                        consumer_list[consumer], "-g", label="Consumer", alpha=line_alpha
-                    )
-                    consumer_label_set = True
+            for consumer in consumers:
+                if best_consumer == consumer:
+                    plt.plot(consumer_list[consumer], "-b", label="Best Consumer")
                 else:
-                    plt.plot(consumer_list[consumer], "-g", alpha=line_alpha)
+                    if not consumer_label_set:
+                        plt.plot(
+                            consumer_list[consumer],
+                            "-g",
+                            label="Consumer",
+                            alpha=line_alpha,
+                        )
+                        consumer_label_set = True
+                    else:
+                        plt.plot(consumer_list[consumer], "-g", alpha=line_alpha)
 
-        for provider in providers:
-            if best_provider == provider:
-                plt.plot(provider_list[provider], ":c", label="Best Provider")
-            else:
-                if not provider_label_set:
-                    plt.plot(
-                        provider_list[provider], ":r", label="Provider", alpha=line_alpha
-                    )
-                    provider_label_set = True
+            for provider in providers:
+                if best_provider == provider:
+                    plt.plot(provider_list[provider], ":c", label="Best Provider")
                 else:
-                    plt.plot(provider_list[provider], ":r", alpha=line_alpha)
+                    if not provider_label_set:
+                        plt.plot(
+                            provider_list[provider],
+                            ":r",
+                            label="Provider",
+                            alpha=line_alpha,
+                        )
+                        provider_label_set = True
+                    else:
+                        plt.plot(provider_list[provider], ":r", alpha=line_alpha)
 
-        profiler.stop("single run")
-        plt.title("Trust Simulation")
-        plt.xlabel("Time step")
-        plt.ylabel("Accumulated Utility")
-        plt.legend()
-        if plot_run:
-            plt.show()
+            profiler.stop("single run")
+            plt.title(f"Trust Simulation - {s} - {name_of(ntcm_type)}")
+            plt.xlabel("Time step")
+            plt.ylabel("Accumulated Utility")
+            plt.legend()
+            if plot_run:
+                plt.show()
 
-        consumer_mae_data_one_run = []
-        for c in consumers:
-            plt.plot(c.MAE)
-            consumer_mae_data_one_run.append(c.MAE)
-        if plot_run:
-            plt.show()
+            consumer_mae_data_one_run = []
+            for c in consumers:
+                plt.plot(c.MAE)
+                consumer_mae_data_one_run.append(c.MAE)
+            if plot_run:
+                plt.show()
 
-        # collect data for export
-        consumer_mae_data_one_run = pd.DataFrame(consumer_mae_data_one_run).stack().reset_index().rename(
-            columns={"level_0": "consumer_index", "level_1": "simulation_run_index", 0: "mae"}
-        )
-        consumer_mae_data_one_run['overall_run_index'] = overall_run_index
-        consumer_mae_data = pd.concat([consumer_mae_data, consumer_mae_data_one_run], ignore_index=True)
-
-    consumer_utility_data.to_csv(experiment_name + "consumer_utility_data.csv")
-    provider_utility_data.to_csv(experiment_name + "provider_utility_data.csv")
-    consumer_mae_data.to_csv(experiment_name + "consumer_mae_data.csv")
+            # collect data for export
+            consumer_mae_data_one_run = (
+                pd.DataFrame(consumer_mae_data_one_run)
+                .stack()
+                .reset_index()
+                .rename(
+                    columns={
+                        "level_0": "consumer_index",
+                        "level_1": "simulation_run_index",
+                        0: "mae",
+                    }
+                )
+            )
+            consumer_mae_data_one_run["overall_run_index"] = overall_run_index
+            consumer_mae_data = pd.concat(
+                [consumer_mae_data, consumer_mae_data_one_run], ignore_index=True
+            )
+        name = name_of(ntcm_type)
+        consumer_utility_data.to_csv(name + "_" + s + "_consumer_utility_data.csv")
+        provider_utility_data.to_csv(name + "_" + s + "_provider_utility_data.csv")
+        consumer_mae_data.to_csv(name + "_" + s + "_consumer_mae_data.csv")
 
     profiler.stop()
     profiler.show()
--- to_trust/settings.py
+++ to_trust/settings.py
@@ -1,12 +1,19 @@
-from methods import ITEA, Act, Travos, MET
-from scenarios import FireWitness, HostileEnvironment, RecruitWitness, StartLying
-from testbed import Simulation
 from to_trust import LyingMode
+from to_trust.methods import ITEA, MET, Act, Travos
+from to_trust.scenarios import (
+    FireProvider,
+    FireWitness,
+    HostileEnvironment,
+    RecruitProvider,
+    RecruitWitness,
+    StartLying,
+)
+from to_trust.testbed import Simulation
 
 Scenario_type = StartLying
-epochs = 2
-runs = 3
-ntcm_type = Travos
+epochs = 1
+runs = 50
+ntcm_type = Act
 
 # Plotting settings
 plot_run = True
@@ -16,8 +23,29 @@ line_alpha = 0.2
 # general settings
 general_parameters = {
     "witness_amount": 30,
+    # "witness_options": {
+    #     "bonus": None,
+    #     "honesty": None,
+    #     "honesty_step": None,
+    #     "ballot_stuffing": None,
+    #     "lying_mode": None,
+    #     "bad_mouthing": None,
+    #     "starts_lying": None,
+    #     "epochs_before_dishonest": None,
+    # },
     "consumer_amount": 1 if ntcm_type is Travos else 5,
     "provider_amount": 50,
+    "provider_options": {
+        "chance": None,
+        "quality": None,
+        "cost": None,
+        "l_chance": 0.5,
+        "u_chance": 0.85,
+        "l_quality": 0.75,
+        "u_quality": 0.99,
+        "l_cost": 0.1,
+        "u_cost": 0.3,
+    },
 }
 
 # A witness is “recruited” into a collusive ring
@@ -27,16 +55,18 @@ recruit_witness_parameters = {
     # witness/provider ratio in ring
     "witness_percentage_of_ring": 0.3,
     # chance for each iteration to add a member to the ring
-    "add_member_chance": 0.2
+    "add_member_chance": 0.2,
 }
-collusive_witness_recruited = RecruitWitness(**general_parameters, **recruit_witness_parameters)
+collusive_witness_recruited = RecruitWitness(
+    **general_parameters, **recruit_witness_parameters
+)
 
 # A witness is “fired” from a collusive ring
 fire_witness_parameters = {
     # size of the ring
     "ring_size": 10,
     # chance for each iteration to fire a member from the ring
-    "fire_member_chance": 0.2
+    "fire_member_chance": 0.2,
 }
 collusive_witness_fired = FireWitness(**general_parameters, **fire_witness_parameters)
 
@@ -44,21 +74,25 @@ collusive_witness_fired = FireWitness(**general_parameters, **fire_witness_param
 recruit_provider_parameters = {
     # size of the ring
     "ring_size": 5,
-    # provider/provider ratio in ring
-    "provider_percentage_of_ring": 0.3,
+    # witness/provider ratio in ring
+    "witness_percentage_of_ring": 0.3,
     # chance for each iteration to add a member to the ring
-    "add_member_chance": 0.2
+    "add_member_chance": 0.2,
 }
-collusive_provider_recruited = RecruitWitness(**general_parameters, **recruit_provider_parameters)
+collusive_provider_recruited = RecruitProvider(
+    **general_parameters, **recruit_provider_parameters
+)
 
 # A provider is “fired” from a collusive ring
 fire_provider_parameters = {
     # size of the ring
     "ring_size": 10,
     # chance for each iteration to fire a member from the ring
-    "fire_member_chance": 0.2
+    "fire_member_chance": 0.2,
 }
-collusive_provider_fired = FireWitness(**general_parameters, **fire_provider_parameters)
+collusive_provider_fired = FireProvider(
+    **general_parameters, **fire_provider_parameters
+)
 
 # Multiple collusive rings
 # TODO: Chloe
@@ -78,25 +112,31 @@ bs_bm_hostile_20_parameters = {
     "lying_mode": LyingMode.Bonus,
     # Amount of bonus added/subtracted if lying mode is bonus
     # Amount given if lying mode is fixed
-    "bonus": .4,
+    "bonus": 0.4,
 }
-bs_bm_hostile_20 = HostileEnvironment(**general_parameters, ** bs_bm_hostile_20_parameters)
+bs_bm_hostile_20 = HostileEnvironment(
+    **general_parameters, **bs_bm_hostile_20_parameters
+)
 
 bs_bm_hostile_40_parameters = {
     "bm_pct": 0.2,
     "bs_pct": 0.2,
     "lying_mode": LyingMode.Bonus,
-    "bonus": .4,
+    "bonus": 0.4,
 }
-bs_bm_hostile_40 = HostileEnvironment(**general_parameters, ** bs_bm_hostile_40_parameters)
+bs_bm_hostile_40 = HostileEnvironment(
+    **general_parameters, **bs_bm_hostile_40_parameters
+)
 
 bs_bm_hostile_80_parameters = {
     "bm_pct": 0.4,
     "bs_pct": 0.4,
     "lying_mode": LyingMode.Bonus,
-    "bonus": .4,
+    "bonus": 0.4,
 }
-bs_bm_hostile_80 = HostileEnvironment(**general_parameters, ** bs_bm_hostile_80_parameters)
+bs_bm_hostile_80 = HostileEnvironment(
+    **general_parameters, **bs_bm_hostile_80_parameters
+)
 
 scenarios = {
     "collusive_witness_recruited": collusive_witness_recruited,
